3f5e1044c16b220fe387a3be65ced0a0b67722fd
michael dilulo <snoleopard01001101@gmail.com>
2020-09-07 13:49:06 -0700

    Navbar - Picture & app.js add for Tribute page

---
+++ app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var navLinks = document.getElementById("tributeLinks");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "block"
+    }
+}
