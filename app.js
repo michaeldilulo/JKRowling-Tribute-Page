0018af8ffcd1fd66b0043092bf08b04eaa22d049
michael dilulo <snoleopard01001101@gmail.com>
2020-09-10 17:55:59 -0700

    HTML CSS, image smooth scroll function name change

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function scrollBarFunction() {
     var navLinks = document.getElementById("tributeLinks");
     if (navLinks.style.display === "block") {
         navLinks.style.display = "none";
